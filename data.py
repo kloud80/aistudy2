9bd7d4668bc2f2d2e6b9557af8a2a94a15652ff8
Kloud <kloud80@gmail.com>
2023-04-16 22:24:56 +0900

    init

--- data.py
+++ data.py
@@ -19,8 +19,51 @@ data.dtypes
 data['거래금액(만원)'].describe()
 data['전용면적(㎡)'].describe()
 
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
-from sklearn import linear_model
+lm = LinearRegression()
+
+X = np.array(data['전용면적(㎡)'].values)
+y = np.array(data['거래금액(만원)'].values)
+
+X = X.reshape([X.shape[0], 1])
+y = y.reshape([y.shape[0], 1])
+
+lm.fit(X, y)
+
+# 거래금액 = 전용면적 * [[1462.40190762]] + [-7466.66266318]
+
+from sklearn.metrics import r2_score
+
+y_pred = lm.predict(X)
+r2_score(y, y_pred)
+
+import matplotlib.pyplot as plt
+
+plt.scatter(y, y_pred)
+plt.show()
+
+잔차 = y - y_pred
+
+plt.plot(잔차)
+
+plt.hist(잔차, bins=100)
+plt.show()
 
 
+y_평균잔차 = y - y.mean()
+
+plt.hist(y_평균잔차, bins=100)
+plt.show()
+
+TERROR = y_평균잔차 * y_평균잔차
+TERROR.sum()
+
+EERROR = 잔차 * 잔차
+EERROR.sum()
+
+설명해낸에러 = TERROR.sum() - EERROR.sum()
+
+설명해낸에러 / TERROR.sum()
 
