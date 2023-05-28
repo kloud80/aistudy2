f37f5baf6415e5e97b1a45dbe2f4cfea5c992493
Kloud <kloud80@gmail.com>
2023-05-28 23:19:16 +0900

    aaa

--- 230528 데이터.py
+++ 230528 데이터.py
@@ -63,4 +63,29 @@ data['시구구명'] = data['시군구'].apply(lambda x : x.split(' ')[1])
 
 data['시구구명']  = data['시군구'].apply(lambda x : x.split(' ')[1])
 
+data = data.reset_index()
+data.drop(columns='index', inplace=True)
 
+datapv=data.pivot_table(index='index', columns='시구구명', values='시군구', aggfunc='count')
+datapv= datapv.fillna(0)
+
+data_ch = pd.concat([data, datapv], axis=1)
+
+
+data_ch.columns.values[19:]
+
+data_ch.columns.values[19:]].values
+
+data_ch[data_ch.columns.values[19:]]
+
+
+cols = np.array(['전용면적(㎡)', '층', '년식', '년식제곱'])
+cols = np.concatenate([cols, np.array(data_ch.columns.values[19:])])
+
+X = np.array(data_ch[cols].values)
+y = np.array(data_ch['거래금액(만원)'].values)
+y = y.reshape([y.shape[0],1])
+
+
+results = sm.OLS(y, X).fit()
+print(results.summary())
