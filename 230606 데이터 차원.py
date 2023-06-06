1e9c20255de03a2e543e0305040de9ce138725bf
Kloud <kloud80@gmail.com>
2023-06-06 22:33:39 +0900

    aaa

--- 230606 데이터 차원.py
+++ 230606 데이터 차원.py
@@ -27,6 +27,8 @@ data['거래금액(만원)'] = data['거래금액(만원)'].str.replace(',', '')
 data['전용면적(㎡)'] = data['전용면적(㎡)'].astype('float')
 data['층'] = data['층'].astype('int')
 data['건축년도'] = data['건축년도'].astype('int')
+
+data['단가'] = data['거래금액(만원)'] / data['전용면적(㎡)']
 data.dtypes
 
 
@@ -37,20 +39,27 @@ data['년식제곱'] = data['년식'] **2
 
 
 X = np.array(data[['전용면적(㎡)', '층', '년식', '년식제곱']].values)
-y = np.array(data['거래금액(만원)'].values)
+y = np.array(data['단가'].values)
 y = y.reshape([y.shape[0],1])
 
+X.shape
 
-plt.scatter(x=X[:,0], y=X[:,1], s=5)
+plt.scatter(x=X[:,0], y=X[:,1], s=1)
 plt.show()
 
+plt.scatter(x=X[:,0], y=X[:,1], c=y, cmap='Reds', s=5)
+plt.show()
+
+
 # k-means clustering 실행
-kmeans = KMeans(n_clusters=6)
-kmeans.fit(X[:, :1])
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(X[:, :2])
+group = kmeans.labels_
 
-
-
+center_k = np.array(kmeans.cluster_centers_)
+center_k.shape
 plt.scatter(x=X[:,0], y=X[:,1], s=5, c=kmeans.labels_, cmap='Accent')
+plt.scatter(x=center_k[:,0], y=center_k[:,1], s=20, c='black')
 plt.show()
 
 
